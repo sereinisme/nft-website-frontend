8f2b14021fd32b7d36bc2c3610303116ae17b9b0
sereinisme <rizkiselipratama@gmail.com>
2022-03-21 21:56:27 +0700

    roadmap done

--- src/components/DrawSvg.js
+++ src/components/DrawSvg.js
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
+//head
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
-import React, { useLayoutEffect, useRef } from 'react'
-import styled from 'styled-components'
-import Vector from '../Icons/Vector';
-=======
 import React, { useRef, useLayoutEffect } from "react";
 import styled from "styled-components";
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
+import { keyframes } from 'styled-components';
 import Vector from "../Icons/Vector";
->>>>>>> f003191df1483994b2be60ffb1e50bc91919ab44
+//>>>>>>> f003191df1483994b2be60ffb1e50bc91919ab44
 
 const VectorContainer = styled.div`
   position: absolute;
@@ -21,15 +16,33 @@ const VectorContainer = styled.div`
   height: 100%;
   overflow: hidden;
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 svg{
    width: 100%;
    height: 100%;
 }
 `
 
+const Bounce = keyframes`
+from {  transform: translateX(-50%) scale(0.5);   }
+to {  transform: translateX(-50%) scale(1);   }
+`
+
+const Ball = styled.div`
+position: absolute;
+top: 0;
+left: 50%;
+transform: translateX(-50%);
+width: 1.5rem;
+height: 1.5rem;
+border-radius: 50%;
+background-color: ${props => props.theme.text};
+animation: ${Bounce} 0.5s linear infinite alternate;
+`
+
 const DrawSvg = () => {
 const ref = useRef(null);
+const ballRef = useRef(null);
 
 gsap.registerPlugin(ScrollTrigger);
 useLayoutEffect(() => {
@@ -58,58 +71,32 @@ useLayoutEffect(() => {
         // reverse the drawing when the scroll goes up
         svg.style.strokeDashoffset = length - draw;
          
+      },
+      onToggle : self => {
+        if(self.isActive){
+          // console.log("Scrolling is active");
+          ballRef.current.styled.display = 'none';
+        }else{
+          // console.log("Scrolling is not active");
+          ballRef.current.styled.display = 'inline-block';
+        }
       }
     }
   })
 
   return () => {
-    
+    if(t1) t1.kill();
   };
-}, [])
+}, []) ;
 
-=======
-  svg {
-    width: 100%;
-    height: 100%;
-  }
-`;
 
-const DrawSvg = () => {
-  const ref = useRef(null);
-
-  gsap.registerPlugin(ScrollTrigger);
-  useLayoutEffect(() => {
-    let element = ref.current;
-
-    let svg = document.getElementsByClassName("svg-path")[0];
-
-    const length = svg.getBoundingClientRect();
-
-    // start positioning of svg drawing
-    svg.style.strokeDasharray = length;
-
-    // hide svg before scrolling start
-    svg.style.strokeDashoffset = length;
-
-    let t1 = gsap.timeline({
-      scrollTrigger: {
-        trigger: element,
-        start: "top center",
-        end: "bottom bottom",
-        onUpdate: (self) => {
-          // here we are
-        },
-      },
-    });
-
-    return () => {};
-  }, []);
-
->>>>>>> f003191df1483994b2be60ffb1e50bc91919ab44
   return (
+    <>
+    <Ball ref={ballRef}/>
     <VectorContainer ref={ref}>
       <Vector />
     </VectorContainer>
+    </>
   );
 };
 
